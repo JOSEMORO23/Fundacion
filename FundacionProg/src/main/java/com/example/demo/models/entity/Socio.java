6d5cb5a15c09b051843599632f9f6a0e58fc9f09
JOSEMORO23 <122384278+JOSEMORO23@users.noreply.github.com>
2024-05-30 10:05:08 -0500

    Add files via upload

---
+++ FundacionProg/src/main/java/com/example/demo/models/entity/Socio.java
@@ -0,0 +1,11 @@
+package com.example.demo.models.entity;
+
+import java.io.Serializable;
+
+public class Socio implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
